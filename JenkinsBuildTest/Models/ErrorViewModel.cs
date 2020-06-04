d06ddedbfb792dab03ba22c584d969101b96f0c9
Krzysztof Dziewit <k.dziewit@predictivesolutions.pl>
2020-06-04 14:27:26 +0200

    Add project files.

---
+++ JenkinsBuildTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinsBuildTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
